ac13c4fd8219f073d0324b9ab0bfb857dbce535a
Shamnad PS <Shamnad.p.s@gmail.com>
2017-04-15 11:25:26 +0530

    changes

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import url
 from . import views
-from myapp.user.views import CreateUserView
-from myapp.test.views import CreateTestView
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
@@ -9,6 +7,9 @@ urlpatterns = [
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/(?P<pk>\d+)/delete/$', views.post_delete, name='post_delete'),
-    url(r'^salestracker/regist/$', CreateUserView.as_view()),
-    url(r'^salestracker/testapi/$', CreateTestView.as_view()),
+    url(r'^salestracker/regist/$', views.user_regist, name='user_regist'),
+    url(r'^salestracker/testapi/$', views.user_regist, name='user_regist'),
+	url(r'^salestracker/checkphone/$', views.checkphone, name='checkphone'),
+	url(r'^salestracker/getuserlocation/$', views.getuserlocation, name='getuserlocation'),
+	url(r'^salestracker/saveuserlocation/$', views.saveuserlocation, name='saveuserlocation'),
 ]
